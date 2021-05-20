c6c9d4588b16e751c6504d2ec5b9f2102cbe8ced
Casey Law <caseyjlaw@gmail.com>
2021-05-20 15:40:56 -0700

    redesign of voevent generation via cli

--- event/cli.py
+++ event/cli.py
@@ -8,9 +8,26 @@ def cli():
 @cli.command()
 @click.argument('inname')
 @click.argument('outname')
-def create_voevent(inname, outname):
-    ve = voevent.create_voevent(inname)
+@click.option('--production', type=bool, default=False)
+def create_voevent(inname, outname, production):
+    """ takes json file with key-value pairs for create_voevent function.
+    Required fields: fluence, p_flux, ra, dec, radecerr, dm, dmerr, width, snr, internalname, mjd, importance
+    """
+
+    with open(inname, 'r') as fp:
+        indict = json.load(fp)
+    ve = voevent.create_voevent(production=production, **indict)
     voevent.create_voevent(ve, outname=outname)
+
+
+@cli.command()
+@click.argument('inname')
+@click.argument('destination')
+def send_voevent(inname, destination):
+    """ Read VOEvent XML file and send it somewhere
+    """
+
+    pass
 
 
 @cli.command()
