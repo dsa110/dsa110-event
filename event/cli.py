7dc7554fcb96a5a24fb5579a9799ac9862debd17
Casey Law <caseyjlaw@gmail.com>
2021-05-20 14:40:26 -0700

    maybe working voevent module

--- event/cli.py
+++ event/cli.py
@@ -1,10 +1,17 @@
 import click
-import tns_api_bulk_report
-import caltechdata
+from event import tns_api_bulk_report, caltechdata, voevent
 
 @click.group('dsaevent')
 def cli():
     pass
+
+@cli.command()
+@cli.argument(inname)
+@cli.argument(outname)
+def create_voevent(inname=inname, outname=outname):
+    ve = voevent.create_voevent(inname)
+    voevent.create_voevent(ve, outname=outname)
+
 
 @cli.command()
 @cli.argument(report_filename)
--- event/voevent.py
+++ event/voevent.py
@@ -1,57 +1,91 @@
-import datetime
 import os
-
+import datetime
 import pytz
 import voeventparse as vp
 
 
-def create_voevent():
+def create_voevent(deployment=False, **kwargs):
     """ template syntax for voeventparse creation of voevent
     """
 
-    v = vp.Voevent(stream='astronomy.physics.science.org/super_exciting_events',
-                   stream_id=123, role=vp.definitions.roles.test)
+    required = ['fluence', 'p_flux', 'ra', 'dec', 'radecerr', 'dm', 'dmerr', 'width', 'snr', 'internalname']
+    assert all([k in kwargs for k in required])
+
+    # TODO: set this correctly
+    dt = datetime.datetime.utcnow()  # datetime(yyyy, mm, dd, hh, mm, ss, tzinfo=pytz.utc)  
+
+    # create voevent instance
+    role = vp.definitions.roles.observation if deployment else vp.definitions.roles.test
+    v = vp.Voevent(stream='',  # TODO: check
+                   stream_id=1, role=role)
 
     vp.set_who(v, date=datetime.datetime.utcnow(),
-               author_ivorn="voevent.4pisky.org")
+               author_ivorn="voevent.dsa-110.caltech.org") # TODO: check
 
-    vp.set_author(v, title="4PiSky Testing Node",
-                  shortName="Tim"
+    vp.set_author(v, title="DSA-110 Testing Node",
+                  shortName="Casey"
     )
 
-    int_flux = vp.Param(name='int_flux',
-                        value="2.0e-3",
-                        unit='Janskys',
-                        ucd='em.radio.100-200MHz',
+    fluence = vp.Param(name='fluence',
+                        value=kwargs[fluence],
+                        unit='Jansky ms',
+                        ucd='em.radio.750-1500MHz', # TODO: check
                         dataType='float',
                         ac=False)
-    int_flux.Description = 'Integrated Flux'
+    fluence.Description = 'Fluence'
 
     p_flux = vp.Param(name='peak_flux',
-                      value=1.5e-3,
+                      value=kwargs[p_flux],
                       unit='Janskys',
-                      ucd='em.radio.100-200MHz',
+                      ucd='em.radio.750-1500MHz',
+                      dataType='float',
                       ac=True
     )
     p_flux.Description = 'Peak Flux'
 
-    v.What.append(vp.Group(params=[p_flux, int_flux], name='source_flux'))
+    dm = vp.Param(name="dm",
+                  value=kwargs[dm],
+                  unit="pc/cm^3",
+                  ucd="phys.dispMeasure;em.radio.750-1500MHz" 
+                  dataType='float',
+                  ac=True
+    )
+    dm.Description = 'Dispersion Measure'
+    
+    dmerr = vp.Param(name="dm_error",
+                  value=kwargs[dmerr],
+                  unit="pc/cm^3",
+                  ucd="phys.dispMeasure;em.radio.750-1500MHz" 
+                  dataType='float',
+                  ac=True
+    )
+    dmerr.Description = 'Dispersion Measure error'
 
-    amb_temp = vp.Param(name="amb_temp",
-                        value=15.5,
-                        unit='degrees',
-                        ucd='phys.temperature')
+    width = vp.Param(name="width",
+                     value=kwargs[width],
+                     unit="ms",
+                     ucd="time.duration;src.var.pulse",
+                     dataType='float',
+                     ac=True
+    )
+    width.Description = 'Temporal width of burst'
 
-    amb_temp.Description = "Ambient temperature at telescope"
-    v.What.append(amb_temp)
+    snr = vp.Param(name="snr",
+                     value=kwargs[snr],
+                     ucd="stat.snr",
+                     dataType='float',
+                     ac=True
+    )
+    snr.Description = 'Signal to noise ratio'
+    
+    v.What.append(vp.Group(params=[p_flux, fluence, dm, dmerr, width, snr], name='event parameters'))
 
     vp.add_where_when(v,
-                      coords=vp.Position2D(ra=123.5, dec=45, err=0.1,
+                      coords=vp.Position2D(ra=kwargs[ra], dec=kwargs[dec], err=kwargs[radecerr],
                                            units='deg',
                                            system=vp.definitions.sky_coord_system.utc_fk5_geo),
-                      obs_time=datetime.datetime(2013, 1, 31, 12, 5, 30,
-                                                 tzinfo=pytz.utc),
-                      observatory_location=vp.definitions.observatory_location.geosurface)
+                      obs_time=dt
+                      observatory_location='OVRO')
 
     print("\n***Here is your WhereWhen:***\n")
     print(vp.prettystr(v.WhereWhen))
@@ -59,10 +93,10 @@ def create_voevent():
     print("\n***And your What:***\n")
     print(vp.prettystr(v.What))
 
-    vp.add_how(v, descriptions='Discovered via 4PiSky',
-               references=vp.Reference('http://4pisky.org'))
+    vp.add_how(v, descriptions='Discovered with DSA-110',
+               references=vp.Reference('http://deepsynoptic.org'))
 
-    vp.add_why(v, name='')
+    vp.add_why(v, name=internalname, description='New candidate FRB')
 
     vp.assert_valid_as_v2_0(v)
 
