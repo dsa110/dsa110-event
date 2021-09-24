7c0cc7a696e125057a7b542557b9c7aabd95801d
ubuntu <ubuntu@pm.me>
2021-09-24 00:17:33 +0000

    moving changes over from dsaT3/labels.py

--- event/labels.py
+++ event/labels.py
@@ -1,18 +1,65 @@
 import json
 import os.path
+import numpy as np
+import subprocess
+import pipes
 
-_allowed = ['astrophysical', 'instrumental', 'unsure/noise', 'rfi', 'archive']
+_allowed = ['astrophysical', 'instrumental', 'unsure/noise', 'rfi', 'save', '']
+
+
+def exists_remote(host, path):
+    """Test if a file exists at path on a host accessible with SSH."""
+    status = subprocess.call(
+        ['ssh', host, 'test -f {}'.format(pipes.quote(path))])
+    if status == 0:
+        return True
+    if status == 1:
+        return False
+    raise Exception('SSH failed')
+
+
+def check_voltages(candname):
+
+    filename = f'/home/ubuntu/data/T3/{candname}.json'
+    assert os.path.exists(filename), f'candidate json file {filename} not found'
+    dd = readfile(filename)
+    
+    corrs = ['corr03','corr04','corr05','corr06','corr07','corr08','corr10','corr11','corr12','corr14','corr15','corr16','corr18','corr19','corr21','corr22']
+
+    # edit corr03_data and corr03_header
+    for corr in corrs:
+
+        data_file = '/home/ubuntu/data/'+candname+'_data.out'
+        header_file = '/home/ubuntu/data/'+candname+'_header.json'
+        if exists_remote(corr+'.sas.pvt', data_file):
+            dd[corr+'_data'] = True
+            print('Found data:', corr)
+        if exists_remote(corr+'.sas.pvt', header_file):
+            dd[corr+'_header'] = True
+            print('Found header:', corr)
+
+    writefile(dd, filename)
+
 
 def readfile(filename):
     """ Read candidate json trigger file and return dict
+    Also accepts npy file
     TODO: add file lock?
     """
 
-    with open(filename, 'r') as fp:
-        dd = json.load(fp)
-
-    return dd
-
+    try:
+        with open(filename, 'r') as fp:
+            dd = json.load(fp)
+        return dd
+    except:
+        print('File is not json')
+    try:
+        dd = np.load(filename,allow_pickle=True)
+        return dd.tolist()
+    except:
+        print('File is not .npy')
+        return None
+        
 
 def writefile(dd, filename):
     """ Write candidate json trigger file with dict
@@ -48,20 +95,34 @@ def set_label(candname, label, filename=None):
     assert label in _allowed, f'label must be in {_allowed}'
 
     if filename is None:
-        filename = f'{candname}.json'
+        filename = f'/home/ubuntu/data/T3/{candname}.json'
 
     assert os.path.exists(filename), f'candidate json file {filename} not found'
 
     dd = readfile(filename)
 
-    if candname in dd.keys():
-        if label == 'save':
-            dd[candname][label] = True
-        else:
-            dd[candname]['label'] = label
-        writefile(dd, filename)
+    if label == 'save':
+        dd[label] = True
     else:
-        print(f'candname {candname} not found in {filename}. no label applied.')
+        dd['label'] = label
+    writefile(dd, filename)
+        
+        
+def set_notes(candname, notes, filename=None):
+    """ Read, add notes, and write candidate json file.
+    Can optionally provide full path to file.
+    Default assumes name of <candname>.json in cwd.
+    TODO: decide if file can have more than one candname.
+    """
+
+    if filename is None:
+        filename = f'/home/ubuntu/data/T3/{candname}.json'
+
+    assert os.path.exists(filename), f'candidate json file {filename} not found'
+
+    dd = readfile(filename)
+    dd['notes'] = notes
+    writefile(dd, filename)
 
 
 def set_probability(candname, probability, filename=None):
@@ -72,17 +133,11 @@ def set_probability(candname, probability, filename=None):
     """
 
     if filename is None:
-        filename = f'{candname}.json'
+        filename = f'/home/ubuntu/data/T3/{candname}.json'
 
     assert os.path.exists(filename), f'candidate json file {filename} not found'
 
     dd = readfile(filename)
 
-    if candname in dd.keys():
-        dd[candname]['probability'] = probability
-        writefile(dd, filename)
-    else:
-        print(f'candname {candname} not found in {filename}. no probability set.')
-        
-        
-
+    dd[candname]['probability'] = probability
+    writefile(dd, filename)
