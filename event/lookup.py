a9ff55c1329bbf381979ae7575a9e2ee74d11069
Casey Law <caseyjlaw@gmail.com>
2021-09-30 15:51:33 -0700

    does both nvss/atnf lookups

--- event/lookup.py
+++ event/lookup.py
@@ -2,7 +2,7 @@ from astropy import units, coordinates, table
 import pickle
 import os
 
-def find_associations(ra, dec, mode='nvss', nvss_radius=5, nvss_flux=400, atnf_radius=5,
+def find_associations(ra, dec, mode='both', nvss_radius=60, nvss_flux=400, atnf_radius=60,
                       nvsscat='nvss_astropy.pkl', atnfcat='atnfcat_v1.56.txt'):
     """ Find cataloged NVSS/pulsar sources near (RA, Dec) in degrees.
     Major check is for bright NVSS sources during VLASS (mode='nvss')
@@ -12,34 +12,38 @@ def find_associations(ra, dec, mode='nvss', nvss_radius=5, nvss_flux=400, atnf_r
     """
 
     workdir = ''
-    assert mode.lower() in ['pulsar', 'nvss']
+    assert mode.lower() in ['pulsar', 'nvss', 'both']
 
-    if os.path.exists(workdir + nvsscat) and mode.lower() == 'nvss':
+    if os.path.exists(workdir + nvsscat) and mode.lower() in ['nvss', 'both']:
         print("Loading NVSS catalog")
         with open(workdir + nvsscat, 'rb') as pkl:
-            catalog = pickle.load(pkl)
-            fluxes = pickle.load(pkl)
-    elif os.path.exists(workdir + atnfcat) and mode.lower() == 'pulsar':
+            catalogn = pickle.load(pkl)
+            fluxesn = pickle.load(pkl)
+    else:
+        print(f"No catalog found for mode {mode} in {workdir}")
+        return None
+
+    if os.path.exists(workdir + atnfcat) and mode.lower() in ['pulsar', 'both']:
         print("Loading ATNF catalog")
         tab = table.Table.read(workdir + atnfcat, format='ascii')
-        catalog = coordinates.SkyCoord(ra=tab['RAJ'], dec=tab['DECJ'], unit=(units.hourangle, units.deg))
+        cataloga = coordinates.SkyCoord(ra=tab['RAJ'], dec=tab['DECJ'], unit=(units.hourangle, units.deg))
     else:
         print(f"No catalog found for mode {mode} in {workdir}")
         return None
 
     coord = coordinates.SkyCoord(ra, dec, unit='deg')
+    associations = []
 
-    if mode.lower() == 'nvss':
+    if mode.lower() in ['nvss', 'both']:
         print("Comparing SkyCoord for candidates to NVSS.")
-        ind, sep2, sep3 = coord.match_to_catalog_sky(catalog)
-        if sep2.value < nvss_radius and fluxes[ind] > nvss_flux:
-            return True
-        else:
-            return False
-    elif mode.lower() == 'pulsar':
+        ind, sep2, sep3 = coord.match_to_catalog_sky(catalogn)
+        if sep2.value < nvss_radius and fluxesn[ind] > nvss_flux:
+            associations.append(catalogn[ind], sep2.value)
+
+    if mode.lower() in ['pulsar', 'both']:
         print("Comparing SkyCoord for candidates to ATNF.")
-        ind, sep2, sep3 = coord.match_to_catalog_sky(catalog)
+        ind, sep2, sep3 = coord.match_to_catalog_sky(cataloga)
         if sep2.value < atnf_radius:
-            return True
-        else:
-            return False
+            associations.append(cataloga[ind], sep2.value)
+
+    return associations
