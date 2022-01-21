f24218f2e63a087f5f105b5a0d185e6ee9a1e36d
ubuntu <ubuntu@pm.me>
2022-01-21 23:05:11 +0000

    adding catalog files and fixing setup.py to deploy it

--- event/lookup.py
+++ event/lookup.py
@@ -2,8 +2,11 @@ from astropy import units, coordinates, table
 import pickle
 import os
 
+_install_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data')
+
+
 def find_associations(ra, dec, mode='both', nvss_radius=60, nvss_flux=400, atnf_radius=60,
-                      nvsscat='nvss_astropy.pkl', atnfcat='atnfcat_v1.56.txt', workdir=''):
+                      nvsscat='nvss_astropy.pkl', atnfcat='atnfcat_v1.56.txt', workdir=_install_dir):
     """ Find cataloged NVSS/pulsar sources near (RA, Dec) in degrees.
     Major check is for bright NVSS sources during VLASS (mode='nvss')
     For mode='pulsar', it will return any pulsar in atnf catalog.
@@ -13,18 +16,18 @@ def find_associations(ra, dec, mode='both', nvss_radius=60, nvss_flux=400, atnf_
 
     assert mode.lower() in ['pulsar', 'nvss', 'both']
 
-    if os.path.exists(workdir + nvsscat) and mode.lower() in ['nvss', 'both']:
+    if os.path.exists(os.path.join(workdir, nvsscat)) and mode.lower() in ['nvss', 'both']:
         print("Loading NVSS catalog")
-        with open(workdir + nvsscat, 'rb') as pkl:
+        with open(os.path.join(workdir, nvsscat), 'rb') as pkl:
             catalogn = pickle.load(pkl)
             fluxesn = pickle.load(pkl)
     else:
         print(f"No catalog found for mode {mode} in {workdir}")
         return None
 
-    if os.path.exists(workdir + atnfcat) and mode.lower() in ['pulsar', 'both']:
+    if os.path.exists(os.path.join(workdir, atnfcat)) and mode.lower() in ['pulsar', 'both']:
         print("Loading ATNF catalog")
-        tab = table.Table.read(workdir + atnfcat, format='ascii')
+        tab = table.Table.read(os.path.join(workdir, atnfcat), format='ascii')
         cataloga = coordinates.SkyCoord(ra=tab['RAJ'], dec=tab['DECJ'], unit=(units.hourangle, units.deg))
     else:
         print(f"No catalog found for mode {mode} in {workdir}")
--- setup.py
+++ setup.py
@@ -6,10 +6,9 @@ setup(name='dsa110-event',
       version=get_git_version(),
       url='http://github.com/dsa110/dsa110-event',
       packages=find_packages(),
-      requirements=['caltechdata_api', 'requests', 'voevent-parse', 'datacite'],
+      package_requirements=['caltechdata_api', 'requests', 'voevent-parse', 'datacite'],
       include_package_data=True,
-#      package_data={'event': ['data/*.json']},
-      data_files=[('event/data', glob.glob('data/*.json'))],
+      data_files=[('event/data', glob.glob('data/*.*'))],
       entry_points='''
         [console_scripts]
         dsaevent=event.cli:cli
