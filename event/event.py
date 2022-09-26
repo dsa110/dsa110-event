e2314023cfcfbe871f35f4d68845e2e27d0978ff
dsa <vikram@caltech.edu>
2022-09-26 20:43:15 +0000

    adding more fields to DSACand

--- event/event.py
+++ event/event.py
@@ -20,13 +20,18 @@ class DSAEvent:
     dec: float
     trigname: str
     gulp: Optional[int] = None
+    filterbank: Optional[str] = None
     filfile: Optional[str] = None
+    filfile_cand: Optional[str] = None
+    filplot_cand: Optional[str] = None
     candplot: Optional[str] = None
     save: Optional[bool] = False
     label: Optional[str] = None
     injected: Optional[bool] = None
     probability: Optional[float] = None
     real: Optional[bool] = None
+    T2_json: Optional[str] = None
+    T2_csv: Optional[str] = None
     corr03_data: Optional[str] = None
     corr03_header: Optional[str] = None
     corr04_data: Optional[str] = None
@@ -67,6 +72,23 @@ class DSAEvent:
     voltage_sb13: Optional[str] = None
     voltage_sb14: Optional[str] = None
     voltage_sb15: Optional[str] = None
+    beamformer_weights: Optional[str] = None
+    beamformer_weights_sb00: Optional[str] = None
+    beamformer_weights_sb01: Optional[str] = None
+    beamformer_weights_sb02: Optional[str] = None
+    beamformer_weights_sb03: Optional[str] = None
+    beamformer_weights_sb04: Optional[str] = None
+    beamformer_weights_sb05: Optional[str] = None
+    beamformer_weights_sb06: Optional[str] = None
+    beamformer_weights_sb07: Optional[str] = None
+    beamformer_weights_sb08: Optional[str] = None
+    beamformer_weights_sb09: Optional[str] = None
+    beamformer_weights_sb10: Optional[str] = None
+    beamformer_weights_sb11: Optional[str] = None
+    beamformer_weights_sb12: Optional[str] = None
+    beamformer_weights_sb13: Optional[str] = None
+    beamformer_weights_sb14: Optional[str] = None
+    beamformer_weights_sb15: Optional[str] = None
     
     def tojson(self):
         """ Convert event dict to json string.
@@ -78,12 +100,18 @@ class DSAEvent:
     def update(self, other):
         """ Update fields of this dsaevent with another dsaevent or dict.
         """
+
         if isinstance(other, DSAEvent):
-            self.__dict__.update(asdict(other))
-        elif isinstance(other, dict):
-            self.__dict__.update(other)
-        else:
-            print('type of other is not recognized (should be dict or DSAEvent')
+            other = asdict(other)
+
+        assert isinstance(other, dict), "update method takes DSACand or dict as argument"
+
+        for kk,vv in other.items():
+            if kk in self.__dict__:
+                if self.__dict__[kk] is None:
+                    self.__dict__[kk] = vv
+            else:
+                print(f"key {kk} not in DSACand")
 
     def writejson(self, outpath=OUTPUT_PATH, lock=None):
         """ Writes event to JSON or updates JSON file that already exists.
