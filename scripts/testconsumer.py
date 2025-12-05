cabfa7a1724343b38227ccf4a8397cffa6400bf4
VR-DSA <vikram@caltech.edu>
2025-12-05 20:43:21 +0000

    adding gcn kafka producer/consumer scripts for testing gcn.py module

---
+++ scripts/testconsumer.py
@@ -0,0 +1,36 @@
+from gcn_kafka import Consumer
+import os
+
+
+if env == 'test':
+    print("using test credentials")
+    domain = 'test.gcn.nasa.gov'
+    client_id = os.environ.get('GCN_ID_PRO_TEST', '')
+    client_secret = os.environ.get('GCN_SECRET_PRO_TEST', '')
+elif env == 'prod':
+    print("using prod credentials")
+    domain = 'gcn.nasa.gov'
+    client_id = os.environ.get('GCN_ID_PRO', '')
+    client_secret = os.environ.get('GCN_SECRET_PRO', '')
+else:
+    print(f"env ({env}) not recognized")
+    sys.exit(1)
+
+# Connect as a consumer (client "dsa110consumer")
+# Warning: don't share the client secret with others.
+consumer = Consumer(client_id=client_id,
+                    client_secret=client_secret,
+                    domain=domain,
+                    config = {'auto.offset.reset': 'earliest'})
+
+# Subscribe to topics and receive alerts
+consumer.subscribe(['gcn.notices.dsa110.frb'])
+while True:
+    for message in consumer.consume(timeout=1):
+        if message.error():
+            print(message.error())
+            continue
+        # Print the topic and message ID
+        print(f'topic={message.topic()}, offset={message.offset()}')
+        value = message.value()
+        print(value)
---
+++ scripts/testproducer.py
@@ -0,0 +1,36 @@
+from gcn_kafka import Producer
+import json
+import os
+
+if env == 'test':
+    print("using test credentials")
+    domain = 'test.gcn.nasa.gov'
+    client_id = os.environ.get('GCN_ID_PRO_TEST', '')
+    client_secret = os.environ.get('GCN_SECRET_PRO_TEST', '')
+elif env == 'prod':
+    print("using prod credentials")
+    domain = 'gcn.nasa.gov'
+    client_id = os.environ.get('GCN_ID_PRO', '')
+    client_secret = os.environ.get('GCN_SECRET_PRO', '')
+else:
+    print(f"env ({env}) not recognized")
+    sys.exit(1)
+
+# Connect as a consumer (client "dsa110producer")
+# Warning: don't share the client secret with others.
+producer = Producer(client_id=client_id,
+                    client_secret=client_secret,
+                    domain=domain)
+
+#jsondict = json.load(jsonfile)
+jsondict = {"event": 'hi'}
+TOPIC = 'gcn.notices.dsa110.frb'
+
+json_data = json.dumps(jsondict).encode("utf-8")
+
+producer.produce(
+	TOPIC,
+	json_data,
+)
+
+producer.flush()
